9e73253128fa19f3c10a87694d25994fe119dd7c
justinnonso05 <123962732+justinnonso05@users.noreply.github.com>
2023-04-18 18:57:11 +0100

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,3 +1,29 @@
+const view = document.getElementById("view");
+const answer = document.getElementById("ans");
+document.getElementById("clear").onclick = function clear() {
+  view.value = "";
+  ans.value = "";
+}
+
+document.getElementById("delete").onclick = function del() {
+  view.value = view.value.slice(0, -1);
+}
+document.getElementById("equals").onclick = function ans() {
+  try {
+    const result = eval(view.value);
+    if (isNaN(result)) {
+      answer.value = "syntaxError";
+    }
+    else {
+      answer.value = result;
+    }
+  }
+  catch (e) {
+    answer.value = 'SyntaxError';
+  }
+}
+
 function input(num) {
-  document.getElementById("view").value=num;
+  ans.value = "";
+  view.value = view.value + num;
 }
